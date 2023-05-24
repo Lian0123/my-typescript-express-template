31543e167206610224676af9a9ebe00fcf833a4b
Lian0123 <lienyungli123@gmail.com>
2023-05-24 08:10:39 +0800

    fix(run-k6-test.sh): docker run command

--- script/shell/run-k6-test.sh
+++ script/shell/run-k6-test.sh
@@ -3,13 +3,14 @@
 if [ ! -f ../envs/.env ] ; then
   export $(cat ./envs/.env | grep SERVICE_HOST | xargs)
   export $(cat ./envs/.env | grep SERVICE_PORT | xargs)
+  export $(cat ./envs/.env | grep NODE_ENV     | xargs)
 
   date=$(date '+%Y_%m_%d_%H_%M_%S')
   docker run --net=host --rm -v "${PWD}:/local" --user $UID  -i grafana/k6 run /local/script/javascript/k6.js \
           --out json=/local/k6/report_$date.json \
           -e SERVICE_HOST=$SERVICE_HOST \
-          -e SERVICE_PORT=$SERVICE_PORT
-          -e NODE_ENV=${NODE_ENV}
+          -e SERVICE_PORT=$SERVICE_PORT \
+          -e NODE_ENV=$NODE_ENV
   
   unset SERVICE_HOST
   unset SERVICE_PORT
